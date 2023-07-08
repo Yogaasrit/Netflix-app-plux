b8043ace05b6e01550e60379302fa308b4bd8233
Yogaasrit <83397145+Yogaasrit@users.noreply.github.com>
2023-07-08 11:29:01 +0530

    Update LifeCycleDemo.js

--- src/Pages/HomePage/LifeCycleDemo/LifeCycleDemo.js
+++ src/Pages/HomePage/LifeCycleDemo/LifeCycleDemo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class LifeCycleDemo extends Component {
   render() {
     return (
-      <div>LifeCycleDemo</div>
+      <div>Life Cycle Demo</div>
     )
   }
 }
