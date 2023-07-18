90919c72842849c5f3b4c98b0b1a084941f6d5e6
Yogaasrit <83397145+Yogaasrit@users.noreply.github.com>
2023-07-18 14:06:59 +0530

    Added own Fragment

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import MockFragment from './MockFragment/MockFragment'
+import { Helmet } from 'react-helmet';
+import ErrorBoundary from '../../Containers/Shared/ErrorBoundary/ErrorBoundary';
 
 const AboutUs = () => {
+  const x = 10;
   return (
-    <div>AboutUs</div>
+  
+    <ErrorBoundary>
+    <h1>AboutUs Page</h1>
+    <h2> Demo of MockFragment page</h2>
+    <MockFragment>
+      <Helmet>
+          <title>About Us </title>
+        </Helmet>
+      {/* <MockFragment><h1>Hi</h1></MockFragment> */}
+      {/* inner component */}
+    <p> Our own fragment of component is serving this UI.</p>
+    <p> Refer MockFragment comp in code base.</p>
+    <p>{x()}</p>
+    </MockFragment>
+    </ErrorBoundary>
   )
 }
 
